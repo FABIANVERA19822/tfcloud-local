3167a445f9105d0c6adefd929ae21a0b625ff145
FABIANVERA19822 <63806446+FABIANVERA19822@users.noreply.github.com>
2021-10-07 18:45:53 -0500

    comit 12

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "0.26.1"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-1"
   version = "3.61.0"
